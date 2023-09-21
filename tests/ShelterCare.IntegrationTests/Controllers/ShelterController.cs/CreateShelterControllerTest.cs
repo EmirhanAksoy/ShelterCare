1e7dc22299cfbb89579933d1af964b1dc5cc2bfc
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-09-21 23:44:22 +0300

    Fake api provided via WireMock lib

--- tests/ShelterCare.IntegrationTests/Controllers/ShelterController.cs/CreateShelterControllerTest.cs
+++ tests/ShelterCare.IntegrationTests/Controllers/ShelterController.cs/CreateShelterControllerTest.cs
@@ -1,5 +1,6 @@
 ﻿using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using ShelterCare.IntegrationTests.FakeApis;
 using ShelterCare.IntegrationTests.ShelterCareApi;
 using Testcontainers.PostgreSql;
 
@@ -7,6 +8,7 @@ namespace ShelterCare.IntegrationTests.Controllers.ShelterController;
 
 public class CreateShelterControllerTest : IClassFixture<ShelterCareApiFactory>,IAsyncLifetime
 {
+    private readonly FakeNationalIdentityApi _fakeNationalIdentityApi = new();
     private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder()
            .WithDatabase("ShelterApiTestDb")
            .WithPassword("admin")
@@ -23,10 +25,13 @@ public class CreateShelterControllerTest : IClassFixture<ShelterCareApiFactory>,
     public async Task InitializeAsync()
     {
         await _postgreSqlContainer.StartAsync();
+        _fakeNationalIdentityApi.SetupEndpoint("1234567890");
+
     }
 
     public async Task DisposeAsync()
     {
+        _fakeNationalIdentityApi.Dispose();
         await _postgreSqlContainer.DisposeAsync();
     }
 }
