828bc9bc41c701a71b8dccb50f7f666ca2bf4913
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-09-21 22:59:14 +0300

    First integration test was added

--- tests/ShelterCare.IntegrationTests/Controllers/ShelterController.cs/CreateShelterControllerTest.cs
+++ tests/ShelterCare.IntegrationTests/Controllers/ShelterController.cs/CreateShelterControllerTest.cs
@@ -1,8 +1,35 @@
-﻿using ShelterCare.IntegrationTests.ShelterCareApi;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using ShelterCare.IntegrationTests.ShelterCareApi;
 
 namespace ShelterCare.IntegrationTests.Controllers.ShelterController;
 
-public class CreateShelterControllerTest : IClassFixture<ShelterCareApiFactory>
+public class CreateShelterControllerTest : IClassFixture<ShelterCareApiFactory>,IAsyncLifetime
 {
+    private readonly IContainer container = new ContainerBuilder()
+            .WithImage("postgres:latest")
+            .WithEnvironment("POSTGRES_USER", "sa")
+            .WithEnvironment("POSTGRES_PASSWORD", "admin")
+            .WithEnvironment("POSTGRES_DB", "ShelterApiTestDb")
+            .WithPortBinding(5555, 5432)
+            .WithWaitStrategy(Wait.ForUnixContainer()
+            .UntilPortIsAvailable(5432))
+            .Build();
 
+
+    [Fact]
+    public async Task Test()
+    {
+        await Task.Delay(300);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await container.DisposeAsync();
+    }
 }
