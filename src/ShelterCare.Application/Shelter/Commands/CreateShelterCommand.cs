56b8369dbf1f78b30beea04371f04d50fddab04e
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-09-24 00:07:24 +0300

    Create shelter command init

---
+++ src/ShelterCare.Application/Shelter/Commands/CreateShelterCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace ShelterCare.Application;
+
+public class CreateShelterCommand : IRequest<Response<Shelter>>
+{
+    public string Name { get; set; } = string.Empty;
+    public string OwnerFullName { get; set; } = string.Empty;
+    public DateTime FoundationDate { get; set; } = DateTime.MinValue;
+    public double TotalAreaInSquareMeters { get; set; } = double.MinValue;
+    public string Address { get; set; } = string.Empty;
+    public string Website { get; set; } = string.Empty;
+}
---
+++ src/ShelterCare.Application/Shelter/Commands/CreateShelterCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using ShelterCare.Core.Abstractions.Repository;
+
+namespace ShelterCare.Application;
+
+public class CreateShelterCommandHandler : IRequestHandler<CreateShelterCommand, Response<Shelter>>
+{
+
+    private readonly IShelterRepository _shelterRepository;
+    private readonly ILogger<CreateShelterCommandHandler> _logger;
+
+    public CreateShelterCommandHandler(IShelterRepository shelterRepository, ILogger<CreateShelterCommandHandler> logger)
+    {
+        _shelterRepository = shelterRepository;
+        _logger = logger;
+    }
+
+
+    public Task<Response<Shelter>> Handle(CreateShelterCommand request, CancellationToken cancellationToken)
+    {
+        // DO VALIDATION 
+
+        // CREATE AND RETURN
+
+        throw new NotImplementedException();
+    }
+}
