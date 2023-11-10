9fcb87104bf558bb9aa5e69d38aeee50e8298547
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-11-11 00:55:53 +0300

    Swagger included in release mode

--- src/ShelterCare.FakeConfirmationApi/Program.cs
+++ src/ShelterCare.FakeConfirmationApi/Program.cs
@@ -11,12 +11,8 @@ List<AnimalUniqueIdentifier>? animalUniqueIdentifiers = builder.Configuration.Ge
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
